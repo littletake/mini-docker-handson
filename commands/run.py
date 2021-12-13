4e9ee200ba0cf16879b8d53fdbb1ef290014ec3f
littletake <ritorut18@gmail.com>
2021-12-13 19:33:28 +0900

    ✨ finish lesson2

--- commands/run.py
+++ commands/run.py
@@ -1,15 +1,17 @@
 import linux
 import os
-
 from typing import Tuple
 
+from commands.cgroup import CGroup
 
-def print_hello(*command):
-    # 第一引数：シェルスクリプトの関数
-    # 第二引数：関数で実行する文字列
+
+def exec_cmd_in_child(*command):
+    # command[0]  : シェルコマンド（str）
+    # command[1:] : 関数で実行する文字列（str）
 
     # PIDの確認
     # print("child: {}".format(os.getpid()))
+    change_hostname(command[1])
 
     if len(command) != 0:
         os.execvp(
@@ -19,14 +21,25 @@ def print_hello(*command):
         # 上記の処理がプロセスを置き換えるためそれ以降の処理がなされない
 
 
-def exec_run(command: Tuple):
+def change_hostname(name: str):
+    print("set hostname: {}".format(name))
+    linux.sethostname(name)
+
+
+def exec_run(command: Tuple, cpu: float):
+    cgroup = CGroup("test")
 
     # linux.clone: 子プロセスの作成
     child_pid = linux.clone(
-        print_hello,
-        0,
+        exec_cmd_in_child,
+        linux.CLONE_NEWUTS,
         command,
     )
+
+    # cpuの制限
+    cgroup.set_cpu_limit(cpu)
+    cgroup.add(child_pid)
+
     os.waitpid(
         child_pid,
         0,
